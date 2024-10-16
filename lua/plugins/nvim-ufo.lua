a9823a8f8660923926c265b7390e935936d85b0a
iProblem <gabrielestudosz@gmail.com>
2024-10-15 21:17:12 -0300

    fix: configurando o nvim-ufo

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -1,36 +1,45 @@
+local handler = function(virtText, lnum, endLnum, width, truncate)
+    local newVirtText = {}
+    local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+    local sufWidth = vim.fn.strdisplaywidth(suffix)
+    local targetWidth = width - sufWidth
+    local curWidth = 0
+    for _, chunk in ipairs(virtText) do
+        local chunkText = chunk[1]
+        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+        if targetWidth > curWidth + chunkWidth then
+            table.insert(newVirtText, chunk)
+        else
+            chunkText = truncate(chunkText, targetWidth - curWidth)
+            local hlGroup = chunk[2]
+            table.insert(newVirtText, { chunkText, hlGroup })
+            chunkWidth = vim.fn.strdisplaywidth(chunkText)
+            if curWidth + chunkWidth < targetWidth then
+                suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+            end
+            break
+        end
+        curWidth = curWidth + chunkWidth
+    end
+    table.insert(newVirtText, { suffix, 'MoreMsg' })
+    return newVirtText
+end
+
 return {
-  {
-    "kevinhwang91/nvim-ufo",
-    dependencies = { "kevinhwang91/promise-async" },
-    event = "BufEnter",
+    'kevinhwang91/nvim-ufo',
+    dependencies = { 'kevinhwang91/promise-async' },
     config = function()
-      -- Fold options
-      vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-      vim.o.foldlevelstart = 99
-      vim.o.foldenable = true
-      require("ufo").setup()
-    end,
-    opts = {
-      preview = {
-        win_config = {
-          border = { "", "─", "", "", "", "─", "", "" },
-          winhighlight = "Normal:Folded",
-          winblend = 0,
-        },
-        mappings = {
-          scrollB = "<C-b>",
-          scrollF = "<C-f>",
-          scrollU = "<C-u>",
-          scrollD = "<C-d>",
-          jumpTop = "[",
-          jumpBot = "]",
-        },
-      },
-      provider_selector = function(_, filetype, buftype)
-        return (filetype == "" or buftype == "nofile") and "indent" -- only use indent until a file is opened
-          or { "treesitter", "indent" } -- if file opened, try to use treesitter if available
-      end,
-    },
-  },
+        local map = require('helpers.keys').map
+        local ufo = require('ufo')
+        map('n', 'zR', ufo.openAllFolds, 'Open all folds')
+        map('n', 'zM', ufo.closeAllFolds, 'Close all folds')
+        map('n', '<leader>zz', 'zA', 'Toggle fold')
+
+        ufo.setup({
+            provider_selector = function()
+                return { 'treesitter', 'indent' }
+            end,
+            fold_virt_text_handler = handler
+        })
+    end
 }
