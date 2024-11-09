329b5cddb1d6328d2b9a6f416007757b9ba61b5b
iProblem <gabrielestudosz@gmail.com>
2024-11-09 16:05:19 -0300

    fix: retirando os indices de profundidade do escopo na coluna da sidebar

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -36,11 +36,13 @@ return {
         map('n', 'zM', ufo.closeAllFolds, 'Close all folds')
         map('n', '<leader>zz', 'zA', 'Toggle fold')
 
-        vim.o.foldcolumn = '1' -- '0' is not bad
-        vim.o.fillchars = [[eob: ,fold: ,foldopen:▾,foldsep: ,foldclose:▸]]
-        vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+
+        vim.o.foldcolumn = '1' -- Mostra a coluna de dobras
+        vim.o.foldlevel = 99 -- Alto valor para garantir que as dobras sejam controladas corretamente
         vim.o.foldlevelstart = 99
         vim.o.foldenable = true
+
+        vim.o.statuscolumn='%=%l%s%{foldlevel(v:lnum) > 0 ? (foldlevel(v:lnum) > foldlevel(v:lnum - 1) ? (foldclosed(v:lnum) == -1 ? "▸" : "▾"): " ") : " " }'
 
         ufo.setup({
             provider_selector = function()
